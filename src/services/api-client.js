62546a033943a14fb9bc1a9ecb206bd8ea56a208
mdasaduzzamanminhaz3 <mdasaduzzamanminhaz3@gmail.com>
2025-12-23 21:25:50 +0600

    add vercel.json file

--- src/services/api-client.js
+++ src/services/api-client.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     baseURL:"https://e-commerce-azure-zeta.vercel.app/api/v1",
+    // baseURL:"http://127.0.0.1:8000/api/v1"
 });
 
 export default apiClient;
